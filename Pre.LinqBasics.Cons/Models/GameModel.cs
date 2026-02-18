be2943ecbb8f723a24f431220a56f983dfb9207c
Mileto Di Marco <mileto.di.marco@howest.be>
2026-02-18 12:03:29 +0100

    Added linq methods up to Select

---
+++ Pre.LinqBasics.Cons/Models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.LinqBasics.Cons.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+    }
+}
--- Pre.LinqBasics.Cons/Program.cs
+++ Pre.LinqBasics.Cons/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.VisualBasic;
+using Pre.LinqBasics.Cons.Models;
 using Pre.LinqBasics.Core;
 using Pre.LinqBasics.Core.Entities;
 using System.Collections;
@@ -11,28 +12,72 @@ Console.WriteLine("Basic linq usage");
 List<string> names = new List<string> {"Mike","Jack","Walter","Farilde","Krista","William","Teddy","Charles","Bronco" };
 List<int> numbers = new List<int> { 2, 4, 5, 9, 89, 12, 2, 5, 78, 3, 4, 5, 5, 56, 9, 87, 21 };
 var games = GetGames();
-foreach(var game in games)
-{
-    Console.WriteLine(game.Title);
-}
 //LINQ SYNTAX
 //Standard query syntax <=> query methods
 var results = from game in games
               where game.Title.Contains("The")
               select game;
-
+PrintLines("query syntax");
+PrintGames(results);
+PrintLines("Query method");
+results = games.Where(g => g.Title.Contains("the"));
+PrintGames(results);
+PrintLines("Single, First, Default");
 //First => FirstOrDefault
-
+var number = numbers.FirstOrDefault(n => n == 666);//
+Console.WriteLine(number);
 //Single => singleOrDefault
-
+number = numbers.SingleOrDefault(n => n == 666);
+//number = numbers.Find(n => n==5); //not Linq
 // Filtering => Where, OfType
-
+PrintLines("Filtering: Where, OfType");
+var namesResult = names.Where(n => n.ToLower().StartsWith("f"));
+PrintNames(namesResult);
+var mixedArray = new ArrayList { "test", 56, 23.00, new Game { Title = "Test" } };
+var gamesFromMixedarray = mixedArray.OfType<Game>();
+PrintGames(gamesFromMixedarray);
 // Sorting => orderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse
-
+PrintLines("Sorting:");
+var orderedNames = names.OrderBy(n => n).Reverse();//alfabetic sort
+var orderedGames = games.OrderBy(g => g.Title).ThenBy(g => g.Published_date).ToList();
+PrintNames(orderedNames);
 // Grouping => GroupBy, ToLookUp
-
+PrintLines("Grouping:GroupBy,ToLookUp");
+var gamesByPublisher = games.GroupBy(g =>g.Type);
+foreach(var item in gamesByPublisher)
+{
+    PrintLines($"{item.Key}");
+    foreach(var game in item)
+    {
+        Console.WriteLine($"{game.Title}");
+    }
+}
 // Projection => Select, SelectMany
+PrintLines("Projection:Select, SelectMany");
+//projectie
+//var gameModelsClassic = new List<GameModel>();
+//foreach(var game in games)
+//{
+//    gameModelsClassic.Add(new GameModel
+//    {
+//        Title = game.Title,
+//        Type = game.Type,
+//        Id = game.Id
+//    });
+//}
+//simple selects projection
+var dlcGameTitles = games.Where(g => g.Type.Equals("DLC")).Select(g => g.Title);
+//simple select projection to anonymous class
+var titleAndDate = games.Where(g => g.Type.Equals("DLC")).Select(g => new {g.Title,g.Published_date });
 
+var gameModels = games
+    .Where(g => g.Type.Equals("DLC"))
+    .Select(g => new GameModel 
+    {
+        Title = g.Title,
+        Type = g.Type,
+        Id = g.Id
+    });
 //Aggregation => Aggregate, Average, Count , Max, Min, Sum,
 
 // Quantifiers => All, Any, Contains
@@ -70,6 +115,14 @@ IEnumerable < Game > GetGames()
     }
     Console.WriteLine(" Games loaded");
     return games.Result;
+}
+//Helper methods
+void PrintNames(IEnumerable<string> names)
+{
+    foreach(var name in names)
+    {
+        Console.Write($"{name} " );
+    }
 }
 void PrintLines(string title)
 {
